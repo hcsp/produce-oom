ea44c3b3e3dda440df8328f136ae092a88a1497d
Bo Zhang <bo@gradle.com>
2019-06-15 17:56:59 +0800

    Update repo

--- src/test/java/com/github/hcsp/objectbasic/MainTest.java
+++ src/test/java/com/github/hcsp/objectbasic/MainTest.java
@@ -1,20 +1,20 @@
-
 package com.github.hcsp.objectbasic;
-org.junit.jupiter.api.Test;;org.junit.jupiter.api.Assertions;
 
-class MainTest {
-@Test
-public void test() {
-    try {
-      Main.main(null);
-    } catch (OutOfMemoryError e) {
-      Assertions.assertTrue(e.getMessage().contains("heap space"));
-      int totalMemory = Runtime.getRuntime().totalMemory();
-      int freeMemory = Runtime.getRuntime().freeMemory();
-      Assertions.assertTrue(totalMemory - freeMemory > freeMemory);
-      return;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void test() {
+        try {
+            Main.main(null);
+        } catch (OutOfMemoryError e) {
+            Assertions.assertTrue(e.getMessage().contains("heap space"));
+            long totalMemory = Runtime.getRuntime().totalMemory();
+            long freeMemory = Runtime.getRuntime().freeMemory();
+            Assertions.assertTrue(totalMemory - freeMemory > freeMemory);
+            return;
+        }
+        Assertions.assertTrue(false);
     }
-    Assertions.assertTrue(false);
-}
-
 }
