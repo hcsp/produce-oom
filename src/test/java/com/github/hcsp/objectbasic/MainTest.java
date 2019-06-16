d665382b46cb43857c8c7d59e1b4bcaf271815f9
Bo Zhang <bo@gradle.com>
2019-06-16 17:21:54 +0800

    Fix failed tests

--- src/test/java/com/github/hcsp/objectbasic/MainTest.java
+++ src/test/java/com/github/hcsp/objectbasic/MainTest.java
@@ -1,20 +1,54 @@
 package com.github.hcsp.objectbasic;
 
+import com.github.hcsp.test.helper.JavaASTReader;
+import com.github.hcsp.test.helper.ProjectSourceFileReader;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.expr.ClassExpr;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
-    public void test() {
+    public void oomTest() {
         try {
             Main.main(null);
         } catch (OutOfMemoryError e) {
             Assertions.assertTrue(e.getMessage().contains("heap space"));
-            long totalMemory = Runtime.getRuntime().totalMemory();
-            long freeMemory = Runtime.getRuntime().freeMemory();
-            Assertions.assertTrue(totalMemory - freeMemory > freeMemory);
             return;
         }
         Assertions.assertTrue(false);
+    }
+
+    @Test
+    public void noExplicitOOM() {
+        // throw new OutOfMemoryError("heap space");
+        Assertions.assertTrue(
+                JavaASTReader.findAll(Main.class, ObjectCreationExpr.class).stream()
+                        .map(ObjectCreationExpr::getType)
+                        .noneMatch(this::isOOME));
+
+        // throw OutOfMemoryError.class.getConstructor(String.class).newInstance("heap space");
+        Assertions.assertTrue(
+                JavaASTReader.findAll(Main.class, ClassExpr.class).stream()
+                        .noneMatch(expr -> expr.getType().asString().contains("OutOfMemoryError")));
+
+        // private static class MyError extends OutOfMemoryError
+        Assertions.assertTrue(
+                JavaASTReader.findAll(Main.class, ClassOrInterfaceDeclaration.class).stream()
+                        .noneMatch(this::extendsOOME));
+
+        // No reflections
+        Assertions.assertFalse(
+                ProjectSourceFileReader.readAsString(Main.class).contains("java.lang.reflect"));
+    }
+
+    private boolean extendsOOME(ClassOrInterfaceDeclaration type) {
+        return type.getExtendedTypes().stream().anyMatch(this::isOOME);
+    }
+
+    private boolean isOOME(ClassOrInterfaceType type) {
+        return "OutOfMemoryError".equals(type.getNameAsString());
     }
 }
