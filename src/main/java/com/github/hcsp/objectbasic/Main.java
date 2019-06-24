324e73d979d65aa76ce20126e5366b7c7444fc1c
wanz <39509865+pandorawanz@users.noreply.github.com>
2019-06-24 19:22:15 +0800

    produce-oom (#14)
    produce-oom (#14)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,15 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        double[] d = new double[1000];
+        List<Object> array = new ArrayList<>();
+        while (true) {
+            array.add(d);
+        }
     }
 }
