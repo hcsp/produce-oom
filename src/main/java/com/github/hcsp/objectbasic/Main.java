07cccc00324d494f2b2bb1c77d2f485e37562d85
༺༽༾ཊ 船长 ཏ༿༼༻ <captaininphw@gmail.com>
2019-07-07 23:46:36 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,9 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        Object[] objArr = new Object[100000];
+        for (int i = 0; i < 100000; i++) {
+            objArr[i] = new Object[100000];
+        }
     }
 }
