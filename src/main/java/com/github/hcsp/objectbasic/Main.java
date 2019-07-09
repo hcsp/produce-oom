226ed69af7e97ae413e051ce79a0edca557eda1a
DeeJayStudy <52043001+DeeJayStudy@users.noreply.github.com>
2019-07-09 17:19:54 +0800

    Update Main.java (#27)
    Update Main.java (#27)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,14 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        ArrayList<Long> arr = new ArrayList<>();
+        for(long i = 0; i < Long.MAX_VALUE; i ++){
+            arr.add(i);
+        }
+        System.out.println(arr);
     }
 }
