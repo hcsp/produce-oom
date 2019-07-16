f9b41c6e623ce93afc2dd3e359aa251d78e29056
Decolo <decolo@163.com>
2019-07-16 21:38:48 +0800

    抛出异常 (#32)
    抛出异常 (#32)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,16 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        int i = 0, j = 0;
+        final int limit = 100_000_000;
+        Byte[][] list = new Byte[limit][limit];
+        while (i < limit) {
+            while (j < limit) {
+                list[i][j] = new Byte("1");
+                j++;
+            }
+            i++;
+        }
+        System.out.println("finish");
     }
 }
