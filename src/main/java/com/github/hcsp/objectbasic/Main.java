c76ecb3dc130ce261a074e73ff8dbe9abb640750
yanghanjie <yang_hanjie@163.com>
2019-08-24 23:51:35 -0400

    让JVM报内存不足异常

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Array;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+
         Byte[] space = new Byte[1024 * 1024 * 1024];
     }
 }
