26df439250621ea1b55ec35f72b1732fc9fa1284
yanghanjie <yang_hanjie@163.com>
2019-08-25 00:05:41 -0400

    照着视频写一下试试能不能过

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -5,7 +5,9 @@ import java.lang.reflect.Array;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
-
-        Byte[] space = new Byte[1024 * 1024 * 1024];
+        Object[] array = new Object[100000];
+        for (int i = 0; i < 1000000; i++) {
+            array[i] = new byte[1024*1024];
+        }
     }
 }
