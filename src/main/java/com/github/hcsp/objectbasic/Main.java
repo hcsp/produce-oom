7b656447ca64e829b8de8cd68ca1f6865f0e976a
Lzy <tydx11@hotmail.com>
2019-09-22 12:36:54 +0800

    抛出OutOfMemoryError

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,13 @@
 package com.github.hcsp.objectbasic;
 
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        Object[] arr = new Object[99999999];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new byte[1024 * 1024 *1024];
+        }
     }
 }
