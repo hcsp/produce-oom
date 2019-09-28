44b1c541095c8e385ffabbd057df8d946ac76ebe
kuanjiang2077 <53144803+kuanjiang2077@users.noreply.github.com>
2019-09-29 00:49:47 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,8 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        bs = new byte[10*1024*1024*1024]
     }
 }
+    
+    
