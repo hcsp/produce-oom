4917341cbcb941d9c55eb1fec0c2c47f2e464ec4
qiaomengnan <2568151682@qq.com>
2019-10-03 16:29:40 +0800

    out

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
-        List<String> values = new ArrayList<>();
+        List<List<String>> values = new ArrayList<>();
         int i = 0;
         while(true) {
-            values.add(new String(i + ""));
-            i++;
+            List<String> data = new ArrayList<>(Integer.MAX_VALUE);
+            values.add(data);
         }
     }
 }
