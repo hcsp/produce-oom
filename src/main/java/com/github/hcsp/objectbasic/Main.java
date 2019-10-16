33f866a94bc240e9fe3bca11afb0662c535ca1c7
GavinYau@1995 <37453766+GavinYau@users.noreply.github.com>
2019-10-16 22:08:26 +0800

    oom test (#83)
    oom test (#83)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,16 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+    private byte[] bytes = new byte[1024 * 1024];
+
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        List<Main> mains = new LinkedList<>();
+        while (true) {
+            mains.add(new Main());
+        }
     }
 }
