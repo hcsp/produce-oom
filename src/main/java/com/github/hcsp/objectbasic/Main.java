77be396f3b57b7921bb6b7aa19a20c05586b42a9
Gav1nYau@1895- <425670133@qq.com>
2019-10-16 22:04:49 +0800

    test

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,15 +1,16 @@
 package com.github.hcsp.objectbasic;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
+    private byte[] bytes = new byte[1024 * 1024];
+
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
-        Map<Integer, String> map = new HashMap<>();
-        int i = 0;
+        List<Main> mains = new LinkedList<>();
         while (true) {
-            map.put(i++, "gavin");
+            mains.add(new Main());
         }
     }
 }
