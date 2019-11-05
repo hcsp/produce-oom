ec925f9fe3784349fc01dd8c974eb98f9c315776
Chris Ji <chrisji0518@outlook.com>
2019-11-05 23:22:12 +0800

    update

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
         Object[] arr = new Object[1000];
-        for (int i = 0; i < 1e10; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = new byte[1024 * 1024];
         }
     }
