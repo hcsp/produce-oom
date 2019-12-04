63500867b86a015bd285fc9a4664058c7a462cd9
zhangsht <z429882801@qq.com>
2019-12-04 12:25:35 +0800

    Java面向对象初步练习：想办法令JVM抛出OutOfMemoryError异常

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,15 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        List<byte[]> list = new ArrayList<>();
+        for (int i = 0; i < 1000000000; i++) {
+            list.add(new byte [1048]);
+        }
+
     }
 }
