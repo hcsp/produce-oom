4b03598a88a78d37dea1d51f7343526503069a4d
Maricaya <43136257+Maricaya@users.noreply.github.com>
2019-12-05 18:39:58 +0800

    fix: produce-oom (#103)
    fix: produce-oom (#103)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,13 @@
 package com.github.hcsp.objectbasic;
 
+import com.sun.corba.se.impl.orbutil.CacheTable;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+            Object[] array = new Object[10000];
+            for (int i = 0; i < 10000; i++) {
+               array[i] = new Integer[1024 * 1024];
+            }
     }
 }
