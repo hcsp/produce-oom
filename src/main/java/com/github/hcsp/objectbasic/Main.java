b643a83d0d2e4568f38035894fe08a0fabab02fc
yaohengfeng <1065435454@qq.com>
2019-12-12 21:15:48 +0800

    报OutOfMemoryError (#107)
    报OutOfMemoryError (#107)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -2,6 +2,10 @@ package com.github.hcsp.objectbasic;
 
 public class Main {
     public static void main(String[] args) {
+        Object[] array = new Object[2000000];
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        for (int i = 0; i < 2000000; i++) {
+            array[i] = new byte[1024 * 1024];
+        }
     }
 }
