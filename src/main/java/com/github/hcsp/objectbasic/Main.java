0564f1761555a60f6674bbd54090b7ed58f06d94
astak16 <1500846601@qq.com>
2019-12-18 10:29:37 +0800

    让jvm抛出OutOfMemoryError错误 (#110)
    让jvm抛出OutOfMemoryError错误 (#110)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,10 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        Object[] array = new Object[10000];
+        for (int i = 0; i <10000; i++) {
+            array[i] = new byte[ 1024 *1024];
+
+        }
     }
 }
