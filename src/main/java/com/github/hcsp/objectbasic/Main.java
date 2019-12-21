b961630ac0b62c837b9083fc7d7d8991fb3cca51
NervousOrange <zch693922442@outlook.com>
2019-12-21 17:23:35 +0100

    from NervousOrange: Assignment 5 of facing the Object.

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,16 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+
+        Object[] a = new Object[10000];
+        for (int i = 0; i <= 10000; i++) {
+            a[i] = new int[1024 * 1024];
+        }
     }
 }
