56b1b69777e49c614960bae3c608656462952c9f
Xi Pu <30915281+XiPu99@users.noreply.github.com>
2019-12-21 16:00:14 -0800

    generate oom error (#113)
    generate oom error (#113)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,17 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < Integer.MAX_VALUE; i++){
+            list.add(null);
+        }
+
     }
 }
