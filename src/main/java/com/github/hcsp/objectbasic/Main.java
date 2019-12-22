b2c2b904610fb1700c0f94e9ae90546eb0b399cf
Jane Wang <janewangcoa@gmail.com>
2019-12-22 15:48:07 +0800

    堆溢出

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,9 +3,9 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
-        Object[] array = new Object[1000];
-        for(int i = 0; i < Integer.MAX_VALUE; i++){
-            array[i] = new long[100];
+        Object[] array = new Object[10000];
+        for (int i = 0; i < 10000; i++) {
+            array[i] = new byte[2333 * 2333];
         }
     }
 
