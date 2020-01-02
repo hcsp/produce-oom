2ab6f48cc09e7e6ba9d851bf1fbfc14093acfbd0
zaker-666 <1075151259china@gmail.com>
2020-01-02 10:54:50 +0800

    抛出异常 (#122)
    抛出异常 (#122)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,13 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        ArrayList<String> arrayList = new ArrayList<>();
+        while (true){
+            arrayList.add("内存快爆了");// 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        }
     }
 }
