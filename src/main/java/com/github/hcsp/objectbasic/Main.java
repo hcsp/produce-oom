f5a9b669076244a5b94aaec0600512d98867b8e6
spclock <58385369+spclock@users.noreply.github.com>
2020-01-04 13:39:40 +0800

    令JVM抛出OutOfMemoryError异常

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -5,6 +5,6 @@ public class Main {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
         List list = new ArrayList();
         for (; ; ) {
-            list.add(new Object());
+            list.add(new int[Integer.MAX_VALUE]);
     }
 }
