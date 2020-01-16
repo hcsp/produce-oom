0dc7582da8c95ff65e3d71a92dacbc4eeefd28ba
王文哲 <mysteryven@gmail.com>
2020-01-16 14:08:26 +0800

    试图 out of memory 错误

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,8 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        for (int i = 0; i < 1e8; i++) {
+            Integer[] j = new Integer[1024 * 1024];
+        }
     }
 }
