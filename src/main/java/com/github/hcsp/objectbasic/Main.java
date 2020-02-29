34601c994009aafc1a3eedeb64236d3000e5db38
Airtoit <51944383+Airtoit@users.noreply.github.com>
2020-03-01 01:14:00 +0800

    令JVM抛出OutOfMemoryError异常 (#150)
    令JVM抛出OutOfMemoryError异常 (#150)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,13 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
-    }
+	public static void main(String[] args) {
+		// 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+		ArrayList list = new ArrayList();
+		while (true) {
+			list.add(new Object());
+		}
+	}
 }
