9bb5d1a570e2acd18ba7fa1f70aa88f02b25f7c4
huiquan99 <53985556+huiquan99@users.noreply.github.com>
2020-03-04 16:46:53 +0800

    out of memory (#155)
    out of memory (#155)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,9 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        int[][] a= new int[1024000][] ;
+        for (int i = 0; i < 1000; i++) {
+            a[i] = new int[10240000];
+        }
     }
 }
