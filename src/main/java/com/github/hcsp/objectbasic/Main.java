3fcca5b69ee489741ebe10a42913d0b4466ca956
arsonalXxu <897386402@qq.com>
2020-03-14 21:27:04 +0800

    内存溢出异常

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,13 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        Object[] obj = new Object[10000];
+        for (int i = 0; i < 10000; i++) {
+            obj[i] = new byte[1024 * 1024];
+        }
     }
 }
