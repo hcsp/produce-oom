2404f52c1fd8e0ea371df3220915f802d3b18f9e
陳雲濤 <36143626+0x522@users.noreply.github.com>
2020-06-28 15:39:08 +0800

    使jvm抛出内存不足的异常。 (#198)
    使jvm抛出内存不足的异常。 (#198)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,12 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        while (true) {
+            int[] a = new int[1024 * 1024 * 1024];
+        }
     }
 }
