0090a99f49ac2783ab914df508660b9bb253e861
xiao <779740337@qq.com>
2020-10-30 13:47:18 +0800

    完成JVM内存空间不足的问题

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
-        Object[] arr = new Object[10000];
+        Object[] arr = new Object[100000];
         for (int i = 0; i < 10000; i++) {
             arr[i] = new byte[1024*1024];
         }
+
     }
 }
