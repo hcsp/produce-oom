c37d6feefdb224e392702928bab00249eaa1aa08
qpWei <42760426+qipengwei@users.noreply.github.com>
2020-11-20 22:46:04 +0800

    GC (#213)
    GC (#213)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,16 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        List<byte[]> list = new ArrayList<>();
+        int i = 0;
+        while (true) {
+            list.add(new byte[100 * (1024 * 1024)]);
+            System.out.println(++i);
+        }
     }
 }
