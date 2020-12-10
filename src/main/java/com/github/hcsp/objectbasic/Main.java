efcc49cb78a5ea977494fafbff48e264bc44b093
yang song <1479867360@qq.com>
2020-12-10 16:33:07 +0800

    抛出OutOfMemoryError异常 (#217)
    抛出OutOfMemoryError异常 (#217)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,13 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Byte[]> list = new ArrayList<>();
+        for (int i = 0; i < 999; i++) {
+            list.add(new Byte[1024*1024*100]);
+        }
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
     }
 }
