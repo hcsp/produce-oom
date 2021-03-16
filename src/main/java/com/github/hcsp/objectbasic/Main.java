107e22ad9ecfcf0f954552cd32b5f7aa88cb3eb2
zhaoyifan0528 <78596080+zhaoyifan0528@users.noreply.github.com>
2021-03-16 10:32:39 +0800

    zyf (#222)
    zyf (#222)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,12 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        while(true)
+        {
+            int []a = new int[1111111121];
+            for (int i = 0; i < 111111111; i++) {
+                a[i] = i;
+            }
+        }
     }
 }
