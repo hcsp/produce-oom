e12a6b5df27d5ce4bb940429279f9cb3b5084cda
Bowen Wu <bwwu@iflytek.com>
2021-06-02 21:55:33 +0800

    feat: get out of memory Error

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,14 @@
 package com.github.hcsp.objectbasic;
 
+import javax.swing.plaf.basic.BasicButtonUI;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        Object[] memoryAllocated = new Byte[100000];
+        int dummyArraySize = 1024 * 1024;
+        for (int i = 0; i < memoryAllocated.length; i++) {
+            memoryAllocated[i] = new Byte[dummyArraySize];
+        }
     }
 }
