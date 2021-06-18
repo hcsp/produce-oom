fbc84692566e41eedf2937b638e6788efce1ca52
jingxin wang <wjxalexander@hotmail.com>
2021-06-18 22:08:07 +0800

    done

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
-        ArrayList<BigInteger> list = new ArrayList<BigInteger>();
-        while(true){
-            list.add(new BigInteger("99999999999999999999999999999999"));
+        Object[] objArr = new Object[1000000];
+        for (int i = 0; i < 100000; i++) {
+            objArr[i] = new Object[1000000];
         }
     }
 }
