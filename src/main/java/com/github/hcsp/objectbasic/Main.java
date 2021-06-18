6c3f4650ec1ae9f10fcfebc2866d936089c508db
jingxin wang <wjxalexander@hotmail.com>
2021-06-18 22:03:57 +0800

    done

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,14 @@
 package com.github.hcsp.objectbasic;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+        while(true){
+            list.add(new BigInteger("99999999999999999999999999999999"));
+        }
     }
 }
