906b12250943e6ecda4f0b26746e20fe165df7b8
lynnyanggod <ouyangyly@gmail.com>
2021-07-21 18:30:42 +0800

    create an OutOfMemoryError

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
         Object[] arr = new Object[10000];
-        for (int i = 0; i < 10000; i++) {
-            arr[i] = new byte[1024*1024];
+        for (int i = 0; i < 1000000000; i++) {
+            arr[i] = new byte[2048 * 2048];
         }
     }
 }
