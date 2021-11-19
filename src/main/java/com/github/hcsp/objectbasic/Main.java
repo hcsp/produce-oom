25ee4d9933c36853b3447898b69f775b187236f6
小黄牛 <changzhengh@icloud.com>
2021-11-19 09:25:56 +0800

    故意用尽内存

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,8 +3,8 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
-        for (int i = 0; i < 100_000_000; i++) {
-            Byte[] bytes = new Byte[1024 * 1024];
+        for (int i = 0; i < 10000000; i++) {
+            Byte[] bytes = new Byte[1024*1024];
         }
     }
 }
