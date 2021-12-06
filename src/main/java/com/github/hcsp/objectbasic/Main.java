8dfdc56caa04339b80b8b2b58d2fe31bad3a954c
jcbound.ma <46434117+jcbound@users.noreply.github.com>
2021-12-06 23:56:14 +0800

    内存溢出 (#261)
    内存溢出 (#261)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,10 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        Object[] array = new Object[1000];
+        for (int i = 0; i < 1024; i++) {
+            array[i] = new Byte[1024 * 1024];
+
+        }
     }
 }
