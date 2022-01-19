dfadda125595a8f206b742753d586fa02b48830d
Hao Jing <92510575+hitjh@users.noreply.github.com>
2022-01-20 00:25:53 +0800

    oom (#262)
    oom (#262)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,6 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        byte[][] b = new byte[100000][100000];
     }
 }
