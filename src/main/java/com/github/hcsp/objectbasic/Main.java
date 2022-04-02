c36985957f0b8964a48aabafc1364f303b99aa1e
bycgxss <bycgxss@gmail.com>
2022-04-02 22:47:53 +0800

    OutOfMemoryError

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -7,15 +7,8 @@ public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
         List<Byte[]> list = new ArrayList<>();
-        int i = 0;
-        try {
-            while (true) {
-                list.add(new Byte[1024 * 1024]);
-                i++;
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
-            System.out.println("执行了"+i+"次");
+        while (true) {
+            list.add(new Byte[1024 * 1024]);
         }
     }
 }
