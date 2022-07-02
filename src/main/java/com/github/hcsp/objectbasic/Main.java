31b13da51bf5b2489094e66216faac9f03256408
陈祥 <indexchen@foxmail.com>
2022-07-02 21:32:37 +0800

    outMemoryError

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -1,7 +1,14 @@
 package com.github.hcsp.objectbasic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+       Object[] A = new Object[100000];
+        for (int i = 0; i < 10000; i++) {
+            A[i] = new char[1024*1024];
+        }
     }
 }
