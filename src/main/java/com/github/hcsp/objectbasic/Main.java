e36b6464daa38e89e6269f120f47a91e6f0686b9
wang <wanggenwang@nuobei.cn>
2022-09-03 18:44:18 +0800

    令JVM抛出OutOfMemoryError异常

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,10 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        Object[] count = new Object[1000000];
+        for (int i = 0; i < 999999; i++) {
+            count[i] = new int[1024*1024];
+        }
+        System.out.println(count);
     }
 }
