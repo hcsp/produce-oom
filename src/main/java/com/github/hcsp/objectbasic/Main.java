f59b8cf563fa8627ee132f382ac0487138487bf9
AndyDrew163 <120002194+AndyDrew163@users.noreply.github.com>
2023-03-19 10:00:39 +0800

    done (#286)
    done (#286)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,5 +3,11 @@ package com.github.hcsp.objectbasic;
 public class Main {
     public static void main(String[] args) {
         // 请想办法在这里写一些代码，占用尽可能多的内存，令JVM抛出内存不足的OutOfMemoryError异常
+        Object[] count = new Object[100000000];
+        int x = 0;
+        while (true) {
+            count[x] = new int[99999999];
+            x += 1;
+        }
     }
 }
